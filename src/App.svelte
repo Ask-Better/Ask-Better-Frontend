00d82e6fe15259c692d152ac8260b5fcb93a8df6
Julian-Mostbauer <julian.mostbauer@outlook.com>
2024-09-21 20:48:37 +0200

    moved functionality from app to util module in questionCard

--- src/App.svelte
+++ src/App.svelte
@@ -4,61 +4,26 @@
   import { type Question } from "./lib/QuestionCard/types";
   import QuestionCard from "./lib/QuestionCard/QuestionCard.svelte";
   import ChatArea from "./lib/Chat/ChatArea.svelte";
-  import { sampleQuestionQuiz, sampleQuestionPoll } from "./lib/QuestionCard/utils";
+  import { backendLogin, exampleQuestions, fetchQuestions } from "./lib/QuestionCard/utils";
 
-  const endPoint = "https://d9kwmlr0-8080.euw.devtunnels.ms/";
-
-  let questions: Question[] = [];
-  const fetchQuestions = async () => {
-    const response = await fetch(endPoint + "api/quiz/list?offset=0");
-    const data = JSON.parse(await response.json());
-
-    console.log(data);
-    return data;
-  };
-
-  const login = async () => {
-    const response = await fetch(endPoint + "api/account/login", {
-      method: "POST",
-      referrerPolicy: "unsafe-url",
-      credentials: "include",
-      headers: {
-        "Access-Controll-Allow-Origin": "*",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        username: "TestUser",
-        passwordHash512:
-          "ee26b0dd4af7e749aa1a8ee3c10ae9923f618980772e473f8819a5d4940e0db27ac185f8a0e1d5f84f88bc887fd67b143732c304cc5fa9ad8e6f57f50028a8ff",
-      }),
-    });
-    console.log(response);
-  };
+  let questions: Question[] = exampleQuestions;
 
   onMount(() => {
-    // login();
-    //
-    // fetchQuestions().then((data) => {
-    // questions = data.slice(0, 5);
-    // console.log(questions);
-    // });
-    
-    // chessBoardSetup();
+    backendLogin();
+
+    fetchQuestions().then((data) => {
+      questions = data.slice(0, 5); // Show only the first 5 questions
+      console.log(questions);
+    });
   });
 </script>
 
 <main>
   <ChatArea />
-  
+
   {#each questions as question}
-  <QuestionCard question={question} />
+    <QuestionCard {question} />
   {/each}
-  
-  {#if questions.length === 0}
-  <p>No questions loaded! Displaying example questions</p>
-  <QuestionCard question={sampleQuestionQuiz} />
-  <QuestionCard question={sampleQuestionPoll} />
-  {/if}
 </main>
 
 <style>
--- src/lib/QuestionCard/utils.ts
+++ src/lib/QuestionCard/utils.ts
@@ -1,21 +1,49 @@
 import { type Question } from "./types";
+const endPoint = "https://d9kwmlr0-8080.euw.devtunnels.ms/";
 
-let sampleQuestionQuiz: Question = {
-  question: "Which of these are mammals?",
-  options: ["Dog", "Cat", "Fish", "Bird", "Human", "Snake"],
-  category: "Biology",
-  allowMultipleAnswers: true,
-  type: "Quiz",
-  correctAnswers: ["Dog", "Cat"],
+const exampleQuestions: Question[] = [
+  {
+    question: "Which of these are mammals?",
+    options: ["Dog", "Cat", "Fish", "Bird", "Human", "Snake"],
+    category: "Biology",
+    allowMultipleAnswers: true,
+    type: "Quiz",
+    correctAnswers: ["Dog", "Cat", "Human"],
+  },
+  {
+    question: "What is your favorite color?",
+    options: ["Red", "Blue", "Green"],
+    category: "Poll",
+    allowMultipleAnswers: false,
+    type: "Poll",
+    correctAnswers: null,
+  },
+];
+
+const fetchQuestions = async () => {
+  const response = await fetch(endPoint + "api/quiz/list?offset=0");
+  const data = JSON.parse(await response.json());
+
+  console.log(data);
+  return data;
 };
 
-let sampleQuestionPoll: Question = {
-  question: "What is your favorite color?",
-  options: ["Red", "Blue", "Green"],
-  category: "Poll",
-  allowMultipleAnswers: false,
-  type: "Poll",
-  correctAnswers: null,
+const backendLogin = async () => {
+  const response = await fetch(endPoint + "api/account/login", {
+    method: "POST",
+    referrerPolicy: "unsafe-url",
+    credentials: "include",
+    headers: {
+      "Access-Controll-Allow-Origin": "*",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      username: "TestUser",
+      passwordHash512:
+        "ee26b0dd4af7e749aa1a8ee3c10ae9923f618980772e473f8819a5d4940e0db27ac185f8a0e1d5f84f88bc887fd67b143732c304cc5fa9ad8e6f57f50028a8ff",
+    }),
+  });
+  console.log(response);
 };
 
-export { sampleQuestionQuiz, sampleQuestionPoll };
+export { exampleQuestions, fetchQuestions, backendLogin };
